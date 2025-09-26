30858298b2e7cbfdbe63c62329d663dbb81c22b8
Shahmeer Abdullah <shahmeerabdullah83@gmail.com>
2025-09-26 10:16:25 +0500

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -6,7 +6,7 @@ int main(){
     int arr[]={5,4,3,2,1};
 
     for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+        for(int j=0; j<5; j++){    
             if(arr[j]>arr[j+1]){
              int temp;
              temp=arr[j];
