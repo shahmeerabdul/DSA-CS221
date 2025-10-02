517b6a5bb6bd81b835ad87d034b862c25675fbb8
shhh-9 <shahmeerabdullah83@gmail.com>
2025-10-02 18:07:39 +0500

    New

---
+++ reversearray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void ReverseArray(int arr[], int size){
+    int start=0;
+    int end=size-1;
+    for(int i=0; i<size/2; i++){
+        int temp;
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+}
+}
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int size=5;
+    ReverseArray(arr,size);
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
