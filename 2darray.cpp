99d72161f3eb430cb8a6e702508486c570014ec5
Shahmeer Abdullah <shahmeerabdullah83@gmail.com>
2025-10-17 15:52:32 +0500

    Update 2darray.cpp

--- 2darray.cpp
+++ 2darray.cpp
@@ -5,7 +5,7 @@ int main(){
     int rows=3;
     int cols=3;
     int matrix[rows][cols];
-
+     
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
             cin>>matrix[i][j];
